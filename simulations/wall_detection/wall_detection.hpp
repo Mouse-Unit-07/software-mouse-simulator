50409bab02b6978e180f98344cac5b8a8059a0d4
Ryuichi Lin <ryuno107lin@gmail.com>
2026-03-25 12:22:14 -0400

    feat: initial wall detection files and folders

---
+++ simulations/wall_detection/wall_detection.hpp
@@ -0,0 +1,24 @@
+/*-------------------------------- FILE INFO ---------------------------------*/
+/* Filename           : wall_detection.hpp                                    */
+/*                                                                            */
+/* Interface to functions to run micromouse wall detection simulation and     */ 
+/* associated config and results analysis helpers                             */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+#ifndef WALL_DETECTION_HPP_
+#define WALL_DETECTION_HPP_
+
+/*----------------------------------------------------------------------------*/
+/*                             Public Definitions                             */
+/*----------------------------------------------------------------------------*/
+/* none */
+
+/*----------------------------------------------------------------------------*/
+/*                             Public Declarations                            */
+/*----------------------------------------------------------------------------*/
+namespace wall_detection
+{
+
+} /* wall_detection namespace */
+
+#endif /* WALL_DETECTION_HPP_ */
