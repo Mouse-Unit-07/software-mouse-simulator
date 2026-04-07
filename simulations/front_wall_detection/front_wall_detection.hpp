3ded56435dc1100ebbb371ad2fd64ecf18d73de6
Ryuichi Lin <ryuno107lin@gmail.com>
2026-04-07 13:51:34 -0400

    feat: add initial front wall detection simulation files and folders

---
+++ simulations/front_wall_detection/front_wall_detection.hpp
@@ -0,0 +1,27 @@
+/*-------------------------------- FILE INFO ---------------------------------*/
+/* Filename           : front_wall_detection.hpp                              */
+/*                                                                            */
+/* Interface to functions to run micromouse front wall detection simulation   */ 
+/* and associated config and results analysis helpers                         */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+#ifndef FRONT_WALL_DETECTION_HPP_
+#define FRONT_WALL_DETECTION_HPP_
+
+/*----------------------------------------------------------------------------*/
+/*                             Public Definitions                             */
+/*----------------------------------------------------------------------------*/
+namespace front_wall_detection
+{
+
+} /* front_wall_detection namespace */
+
+/*----------------------------------------------------------------------------*/
+/*                             Public Declarations                            */
+/*----------------------------------------------------------------------------*/
+namespace front_wall_detection
+{
+
+} /* front_wall_detection namespace */
+
+#endif /* FRONT_WALL_DETECTION_HPP_ */
