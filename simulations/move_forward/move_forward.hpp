246cffcaab6daea76617c5708ed68a7ffa59d7cd
Ryuichi Lin <ryuno107lin@gmail.com>
2026-04-07 11:09:52 -0400

    feat: initial folders and files for move forward simulation

---
+++ simulations/move_forward/move_forward.hpp
@@ -0,0 +1,27 @@
+/*-------------------------------- FILE INFO ---------------------------------*/
+/* Filename           : move_forward.hpp                                      */
+/*                                                                            */
+/* Interface to functions to run micromouse move_forward simulation and       */ 
+/* associated config and results analysis helpers                             */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+#ifndef MOVE_FORWARD_HPP_
+#define MOVE_FORWARD_HPP_
+
+/*----------------------------------------------------------------------------*/
+/*                             Public Definitions                             */
+/*----------------------------------------------------------------------------*/
+namespace move_forward
+{
+
+} /* move_forward namespace */
+
+/*----------------------------------------------------------------------------*/
+/*                             Public Declarations                            */
+/*----------------------------------------------------------------------------*/
+namespace move_forward
+{
+
+} /* move_forward namespace */
+
+#endif /* MOVE_FORWARD_HPP_ */
