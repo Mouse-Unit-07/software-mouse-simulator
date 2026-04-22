f9bfb607749aa3f98a1258d020f8833a3923aae9
Ryuichi Lin <ryuno107lin@gmail.com>
2026-04-22 18:54:08 -0400

    refactor, feat: remove obsolete sweeper/math, update double to filename

--- simulations/common/simulation_common.hpp
+++ simulations/common/simulation_common.hpp
@@ -10,102 +10,7 @@
 /*----------------------------------------------------------------------------*/
 /*                             Public Definitions                             */
 /*----------------------------------------------------------------------------*/
-namespace simulation_common
-{
-
-class CommonConfigSweeper {
-public:
-    void init_sizes(std::vector<size_t> sizes);
-    bool next(void);
-    const std::vector<size_t>& get_indices(void) const;
-
-private:
-    std::vector<size_t> sizes_;
-    std::vector<size_t> indices_;
-    bool first_{true};
-};
-
-template <typename T> std::vector<T> generate_sweep_values(T min, T max, int steps)
-{
-    std::vector<T> values;
-
-    if (steps <= 0) {
-        return values;
-    }
-
-    if (steps == 1) {
-        values.push_back(min);
-        return values;
-    }
-
-    if (steps == 2) {
-        values.push_back(min);
-        values.push_back(max);
-        return values;
-    }
-
-    values.reserve(steps);
-
-    for (int i{0}; i < steps; ++i) {
-        double t{static_cast<double>(i) / (steps - 1)};
-        values.push_back(static_cast<T>(min + (t * (max - min))));
-    }
-
-    return values;
-}
-
-template <typename Trials, typename KeyFn, typename ValueFn>
-auto group_by(const Trials& trials, KeyFn key_fn, ValueFn value_fn)
-{
-    using Key = decltype(key_fn(trials.front()));
-    using Value = decltype(value_fn(trials.front()));
-
-    std::map<Key, std::vector<Value>> grouped;
-
-    for (const auto& t : trials) {
-        grouped[key_fn(t)].push_back(value_fn(t));
-    }
-
-    return grouped;
-}
-
-struct MetricStats {
-    double mean{0.0};
-    double stddev{0.0};
-    double min{0.0};
-    double max{0.0};
-};
-
-template <typename Trials, typename Fn>
-std::vector<double> extract_metric(const Trials& trials, Fn fn)
-{
-    std::vector<double> out;
-    out.reserve(trials.size());
-
-    for (const auto& t : trials) {
-        out.push_back(fn(t));
-    }
-
-    return out;
-}
-
-template <typename Trials, typename Pred> double compute_rate(const Trials& trials, Pred pred)
-{
-    if (trials.empty()) {
-        return 0.0;
-    }
-
-    int count{0};
-    for (const auto& t : trials) {
-        if (pred(t)) {
-            count++;
-        }
-    }
-
-    return static_cast<double>(count) / trials.size();
-}
-
-} /* simulation_common namespace */
+/* none */
 
 /*----------------------------------------------------------------------------*/
 /*                             Public Declarations                            */
@@ -113,11 +18,7 @@ template <typename Trials, typename Pred> double compute_rate(const Trials& tria
 namespace simulation_common
 {
 
-MetricStats compute_stats(const std::vector<double>& data);
-double collapse_metric(const MetricStats& s);
-double safe_norm(double v, double max);
-double compute_metric_score(double collapsed, double global_max);
-std::string double_to_filename(double v, int precision = 2);
+std::string double_to_filename(double v, int precision = 3);
 
 } /* simulation_common namespace */
 
